7e77f1666e99d6cf55ed34b91b899e7f9779dd87
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-15 21:57:52 +0200

    Update product and category routes to use 'id' instead of resource names

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@ Route::get('/', function () {
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/create', [ProductController::class, 'create']);
 Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{product}/edit', [ProductController::class, 'edit']);
-Route::post('/products/{product}', [ProductController::class, 'update']);
-Route::get('/products/{product}/delete', [ProductController::class, 'destroy']);
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+Route::post('/products/{id}', [ProductController::class, 'update']);
+Route::get('/products/{id}/delete', [ProductController::class, 'destroy']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/create', [CategoryController::class, 'create']);
 Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{category}/edit', [CategoryController::class, 'edit']);
-Route::post('/categories/{category}', [CategoryController::class, 'update']);
-Route::get('/categories/{category}/delete', [CategoryController::class, 'destroy']);
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']);
+Route::post('/categories/{id}', [CategoryController::class, 'update']);
+Route::get('/categories/{id}/delete', [CategoryController::class, 'destroy']);
