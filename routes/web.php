fb8680aa4b171e1acb6790d1e482a1aff7b6ccfe
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-03-15 21:56:50 +0200

    Add product and category routes for CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
-use App\Http\Controllers\CategroyController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/create', [ProductController::class, 'create']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}/edit', [ProductController::class, 'edit']);
+Route::post('/products/{product}', [ProductController::class, 'update']);
+Route::get('/products/{product}/delete', [ProductController::class, 'destroy']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/create', [CategoryController::class, 'create']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories/{category}/edit', [CategoryController::class, 'edit']);
+Route::post('/categories/{category}', [CategoryController::class, 'update']);
+Route::get('/categories/{category}/delete', [CategoryController::class, 'destroy']);
